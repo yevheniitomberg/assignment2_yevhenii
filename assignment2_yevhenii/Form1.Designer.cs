e3dcdd5c7b8b3128f3c12b26eb7c9b0394094a31
Yevhenii Tomberg <yevhenii@tomberg.com>
2022-09-22 02:25:33 +0300

    Add project files.

---
+++ assignment2_yevhenii/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace assignment2_yevhenii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.input_c = new System.Windows.Forms.TextBox();
+            this.input_b = new System.Windows.Forms.TextBox();
+            this.input_a = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rev_total = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.class_c_rev_total = new System.Windows.Forms.Label();
+            this.class_b_rev_total = new System.Windows.Forms.Label();
+            this.class_a_rev_total = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.cacl_btn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.input_c);
+            this.groupBox1.Controls.Add(this.input_b);
+            this.groupBox1.Controls.Add(this.input_a);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(33, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 204);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tickets Sold";
+            // 
+            // input_c
+            // 
+            this.input_c.AccessibleName = "input_c";
+            this.input_c.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.input_c.Location = new System.Drawing.Point(128, 138);
+            this.input_c.Name = "input_c";
+            this.input_c.Size = new System.Drawing.Size(100, 26);
+            this.input_c.TabIndex = 5;
+            // 
+            // input_b
+            // 
+            this.input_b.AccessibleName = "input_b";
+            this.input_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.input_b.Location = new System.Drawing.Point(128, 101);
+            this.input_b.Name = "input_b";
+            this.input_b.Size = new System.Drawing.Size(100, 26);
+            this.input_b.TabIndex = 4;
+            // 
+            // input_a
+            // 
+            this.input_a.AccessibleName = "input_a";
+            this.input_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.input_a.Location = new System.Drawing.Point(128, 56);
+            this.input_a.Name = "input_a";
+            this.input_a.Size = new System.Drawing.Size(100, 26);
+            this.input_a.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(46, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Class C:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(46, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Class B:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(46, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Class A:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rev_total);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.class_c_rev_total);
+            this.groupBox3.Controls.Add(this.class_b_rev_total);
+            this.groupBox3.Controls.Add(this.class_a_rev_total);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(334, 33);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(267, 204);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Revenue Generated";
+            // 
+            // rev_total
+            // 
+            this.rev_total.AccessibleName = "rev_total";
+            this.rev_total.AutoSize = true;
+            this.rev_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.rev_total.Location = new System.Drawing.Point(119, 164);
+            this.rev_total.MinimumSize = new System.Drawing.Size(100, 20);
+            this.rev_total.Name = "rev_total";
+            this.rev_total.Size = new System.Drawing.Size(100, 20);
+            this.rev_total.TabIndex = 7;
+            this.rev_total.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label10.Location = new System.Drawing.Point(46, 164);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 20);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Total:";
+            // 
+            // class_c_rev_total
+            // 
+            this.class_c_rev_total.AccessibleName = "class_c_rev_total";
+            this.class_c_rev_total.AutoSize = true;
+            this.class_c_rev_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.class_c_rev_total.Location = new System.Drawing.Point(119, 125);
+            this.class_c_rev_total.MinimumSize = new System.Drawing.Size(100, 20);
+            this.class_c_rev_total.Name = "class_c_rev_total";
+            this.class_c_rev_total.Size = new System.Drawing.Size(100, 20);
+            this.class_c_rev_total.TabIndex = 5;
+            this.class_c_rev_total.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // class_b_rev_total
+            // 
+            this.class_b_rev_total.AccessibleName = "class_b_rev_total";
+            this.class_b_rev_total.AutoSize = true;
+            this.class_b_rev_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.class_b_rev_total.Location = new System.Drawing.Point(119, 85);
+            this.class_b_rev_total.MinimumSize = new System.Drawing.Size(100, 20);
+            this.class_b_rev_total.Name = "class_b_rev_total";
+            this.class_b_rev_total.Size = new System.Drawing.Size(100, 20);
+            this.class_b_rev_total.TabIndex = 4;
+            this.class_b_rev_total.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // class_a_rev_total
+            // 
+            this.class_a_rev_total.AccessibleName = "class_a_rev_total";
+            this.class_a_rev_total.AutoSize = true;
+            this.class_a_rev_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.class_a_rev_total.Location = new System.Drawing.Point(119, 43);
+            this.class_a_rev_total.MinimumSize = new System.Drawing.Size(100, 20);
+            this.class_a_rev_total.Name = "class_a_rev_total";
+            this.class_a_rev_total.Size = new System.Drawing.Size(100, 20);
+            this.class_a_rev_total.TabIndex = 3;
+            this.class_a_rev_total.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(46, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Class C:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(46, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Class B:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(46, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Class A:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label12.Location = new System.Drawing.Point(18, 28);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(211, 20);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "Enter the number of tickets";
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.AccessibleName = "clear_btn";
+            this.clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.clear_btn.Location = new System.Drawing.Point(270, 263);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(86, 30);
+            this.clear_btn.TabIndex = 7;
+            this.clear_btn.Text = "CLEAR";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.AccessibleName = "exit_btn";
+            this.exit_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.exit_btn.Location = new System.Drawing.Point(384, 263);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(75, 30);
+            this.exit_btn.TabIndex = 8;
+            this.exit_btn.Text = "EXIT";
+            this.exit_btn.UseVisualStyleBackColor = true;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // cacl_btn
+            // 
+            this.cacl_btn.AccessibleName = "cacl_btn";
+            this.cacl_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cacl_btn.Location = new System.Drawing.Point(102, 263);
+            this.cacl_btn.Name = "cacl_btn";
+            this.cacl_btn.Size = new System.Drawing.Size(139, 30);
+            this.cacl_btn.TabIndex = 9;
+            this.cacl_btn.Text = "CALCULATE";
+            this.cacl_btn.UseVisualStyleBackColor = true;
+            this.cacl_btn.Click += new System.EventHandler(this.cacl_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 330);
+            this.Controls.Add(this.cacl_btn);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Stadium Seating";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input_c;
+        private System.Windows.Forms.TextBox input_b;
+        private System.Windows.Forms.TextBox input_a;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label rev_total;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label class_c_rev_total;
+        private System.Windows.Forms.Label class_b_rev_total;
+        private System.Windows.Forms.Label class_a_rev_total;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button clear_btn;
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.Button cacl_btn;
+    }
+}
+
---
+++ assignment2_yevhenii/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment2_yevhenii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cacl_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a_rev = Double.Parse(this.input_a.Text) * 15;
+                double b_rev = Double.Parse(this.input_b.Text) * 12;
+                double c_rev = Double.Parse(this.input_c.Text) * 9;
+                this.class_a_rev_total.Text = a_rev + " €";
+                this.class_b_rev_total.Text = b_rev + " €";
+                this.class_c_rev_total.Text = c_rev + " €";
+                this.rev_total.Text = (a_rev + b_rev + c_rev) + " €";
+            } catch (Exception)
+            {
+                MessageBox.Show("Incorrect input type was used");
+            }
+
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            this.input_a.Text = "";
+            this.input_b.Text = "";
+            this.input_c.Text = "";
+            this.rev_total.Text = "";
+            this.class_a_rev_total.Text = "";
+            this.class_b_rev_total.Text = "";
+            this.class_c_rev_total.Text = "";
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
